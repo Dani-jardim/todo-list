7c0a928ec3f4c30b0128fcbbc4660a2eb380a0ff
Daniela Jardim <danialvesdesigner@gmail.com>
2020-02-21 18:09:39 -0300

    add function ItemFeito

--- js/index.js
+++ js/index.js
@@ -7,3 +7,18 @@ function addItemLista(){
 		liItem.appendChild(textoItem);
 		listaItem.appendChild(liItem);
 }
+
+
+
+function feitoItem(){
+var descendentes = document.querySelectorAll("#lista li");
+for (var i = 0; i < descendentes.length; i++) {
+	var atribuir = document.createAttribute("class");
+    descendentes[i].addEventListener("click", function (e) {
+			this.setAttributeNode(atribuir);
+        console.log('O elemento clicado foi o ' + this.innerHTML);
+    })
+}
+}
+
+
