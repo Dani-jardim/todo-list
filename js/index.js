999efe3cc34552d0fc28898a84af870fc216aa44
Daniela Jardim <danialvesdesigner@gmail.com>
2020-02-28 01:07:44 -0300

    adicionando botao fechar depois de cada novo item

--- js/index.js
+++ js/index.js
@@ -9,5 +9,11 @@ function novoItem (){
 		else{
 			document.getElementById("listaCompleta").appendChild(ListaNode);
 		}
-		document.getElementById("listaCompleta").value = "";	
+		document.getElementById("itemLista").value = "";	//apagando a linha do input
+
+		var btn = document.createElement("button"); // add o btn para apagar em cada novo item
+		var txtBtn = document.createTextNode("X") 
+		btn.className = "fechar"; // criar class depois
+		btn.appendChild(txtBtn);
+		ListaNode.appendChild(btn)
 };
