a017fe985aaf1f1e2afc7912e0f88de116535854
Daniela Jardim <danialvesdesigner@gmail.com>
2020-02-28 00:26:20 -0300

    iniciando o projeto novamente. Add a function novo item

--- js/index.js
+++ js/index.js
@@ -1,24 +1,13 @@
-
-function addItemLista(){
-	var item = document.getElementById('item').value,
-		listaItem = document.getElementById('lista'),
-		liItem = document.createElement("li"),
-		textoItem = document.createTextNode(item);
-		liItem.appendChild(textoItem);
-		listaItem.appendChild(liItem);
-}
-
-
-
-function feitoItem(){
-var descendentes = document.querySelectorAll("#lista li");
-for (var i = 0; i < descendentes.length; i++) {
-	var atribuir = document.createAttribute("class");
-    descendentes[i].addEventListener("click", function (e) {
-			this.setAttributeNode(atribuir);
-        console.log('O elemento clicado foi o ' + this.innerHTML);
-    })
-}
-}
-
-
+function novoItem (){
+	var ListaNode = document.createElement("li");
+	var inputItem = document.getElementById("itemLista").value;
+	var textoInput = document.createTextNode(inputItem);
+	ListaNode.appendChild(textoInput);
+	if(inputItem === ""){
+			alert("Voce precisa digitar uma tarefa");
+		} 
+		else{
+			document.getElementById("listaCompleta").appendChild(ListaNode);
+		}
+		document.getElementById("listaCompleta").value = "";	
+};
